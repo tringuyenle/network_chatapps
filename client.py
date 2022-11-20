22cfb1f99f709e7d2dd1e206c0326dbef62ba227
tringuyenle <102273673+tringuyenle@users.noreply.github.com>
2022-11-20 10:11:28 +0700

    update 1.1

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import sys
 import threading
 
-rendezvous = ('147.182.184.215', 55555)
+rendezvous = ('192.168.1.166', 55555)
 
 # connect to rendezvous
 print('connecting to rendezvous server')
